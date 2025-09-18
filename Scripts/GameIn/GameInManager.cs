51f434ffc87ee7c4b8334f755acc6f38b7328dbe
Ouse <2216528769@qq.com>
2025-09-19 00:18:58 +0800

    1

---
+++ Scripts/GameIn/GameInManager.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDemo.Scripts.GameIn
+{
+	public partial class GameInManager:Node2D
+	{
+
+	}
+}
